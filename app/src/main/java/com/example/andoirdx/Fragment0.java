2a087d247a5814a7a3e351596fc89b24976aa9e1
Vorem_dim <ustinov.artem2002@gmail.com>
2023-03-12 18:42:32 +0300

    Fragment replace android navigation

--- app/src/main/java/com/example/andoirdx/Fragment0.java
+++ app/src/main/java/com/example/andoirdx/Fragment0.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Fragment0 extends Fragment {
 
@@ -20,5 +20,12 @@ public class Fragment0 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Button button = view.findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_Fragment0_to_Fragment1);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/andoirdx/Fragment1.java
+++ app/src/main/java/com/example/andoirdx/Fragment1.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.View;
+import android.widget.Button;
 
 public class Fragment1 extends Fragment {
 
@@ -18,5 +20,12 @@ public class Fragment1 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Button button = view.findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_Fragment1_to_Fragment2);
+            }
+        });
     }
 }
